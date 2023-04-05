768c92ef2c86ac69b2baecef65ae8b09df3446cf
raulfr99 <39865952+raulfr99@users.noreply.github.com>
2023-04-04 22:38:07 -0600

    SO changes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,8 @@
+
 const Odoo = require('odoo-xmlrpc')
+const https = require('https');
 require('dotenv').config();
+const axios = require('axios');
 
 const odoo = new Odoo({
     url: 'http://dev-idcerp.mx/xmlrpc/2',
@@ -102,8 +105,11 @@ exports.getAllSales = (req, res, next) => {
         if(err) {return console.log(err)}
         console.log('Connected to Odoo server')
         console.log(req.body.params)
+        var finalArray = []
+        var testArray = []
+
             var inParams = [];
-            inParams.push([['partner_id.id', '=', '182983'],['product_id.id', '=', '4']]);
+            inParams.push([['partner_id.id', '=', '186804'],['product_id.id', '=', '4']]);
             
             // inParams.push([['partner_id.id', '=', req.body.params.userID],['product_id.id', '=', '4']]);
             inParams.push(['name','confirmation_date','partner_id','user_id','amount_total',
@@ -114,40 +120,136 @@ exports.getAllSales = (req, res, next) => {
             params.push(inParams);
             odoo.execute_kw('sale.order', 'search_read', params, function (err2, value) {
                 if (err2) { return console.log(err2); }
-                console.log(value)
+                // console.log(value)
                 let filter = value.filter(val => val.product_id[0] == 4 && val.x_studio_field_DGArF && val.origin)
+                 function callBack () {
+                    var itemsProcessed = 0;
+                    const result = finalArray.map(data =>{
+                        if(filter.some(sort => sort.origin === data.name)){
+                            data.NIP = filter.find(
+                                sort => sort.origin === data.name
+                            ).x_studio_field_DGArF;
+                           
+                            return data;
+                        }
+                        else{
+                            return data;
+                        }
+                    })
+
+                    result.forEach(async (item,index) => {
+                         await axios.get('http://serviciowebidc.idconline.mx/CONSULTORES/API/CLIENTES/'+item.NIP)
+                        .then(res => {
+                            result[index].nipData = res.data
+                            result[index].avaible = true
+
+                            
+                        })
+                        .catch(err => {
+                            result[index].avaible = false
+                            console.log('Error: ', err.message);
+                        });
+                        itemsProcessed++;
+                        if(itemsProcessed === result.length) {
+                            testArray = result
+                            nipCallback();
+                        }
+
+                    })
+                    
+                    // console.log('Construido', testo)
+                    // if(result.length > 0){
+                    //     res.status(200).json({
+                    //         status: "success",
+                    //         length: value?.length,
+                    //         data: result,
+                    //         subscription: true,
+                    //     });
+                    // }else{
+                    //     res.status(200).json({
+                    //         status: "success",
+                    //         length: value?.length,
+                    //         data: result,
+                    //         subscription: false,
+                            
+                    //     });
+                    // }
+                    // console.log('Final: ', finalArray)
+                }
+                 function nipCallback() {
+                    console.log('test: ',testArray)
+                    // if(result.length > 0){
+                        res.status(200).json({
+                            status: "success",
+                            length: value?.length,
+                            data: testArray,
+                            // subscription: true,
+                        });
+                        // }else{
+                            // res.status(200).json({
+                            //     status: "success",
+                            //     length: value?.length,
+                            //     data: result,
+                            //     subscription: false,
+                                
+                            // });
+                        // }
+                }
+
+                var itemsProcessed = 0;
+                filter.forEach(item => {
                 inParams = []
                 params = []
-                inParams.push([['name', '=', filter[0].origin]]);
-                let NIP = filter[0].x_studio_field_DGArF
-                // inParams.push(['id', 'name','display_name','res_name']);
+                inParams.push([['name', '=', item.origin]]);
+                inParams.push(['date_start', 'recurring_next_date','name']);
                 params.push(inParams);
-                
                 odoo.execute_kw('sale.subscription', 'search_read', params, function(err,value){
-                    if (err) { return console.log(err); }
+                if (err) { return console.log(err); }
                     let yourDate = new Date()
                     let formatedDate = yourDate.toISOString().split('T')[0]
-                    if(value[0].recurring_next_date >= formatedDate){
-                        res.status(200).json({
-                            status: "success",
-                            length: value?.length,
-                            data: value,
-                            subscription: true,
-                            NIP: NIP
-                        });
-                    }else{
-                        res.status(200).json({
-                            status: "success",
-                            length: value?.length,
-                            // data: value,
-                            message: "La suscripcion ha vencido!",
-                            subscription: false
-                        });
-                    }
-                    
-                    console.log(value)
-                       
+                if(new Date(value[0].recurring_next_date) >= new Date(formatedDate)){
+                    finalArray.push(value[0])
+                }else{
+
+                }
+                itemsProcessed++;
+                if(itemsProcessed === filter.length) {
+                    callBack();
+                }
                 })
+                })
+                // inParams = []
+                // params = []
+                // inParams.push([['name', '=', filter[0].origin]]);
+                // let NIP = filter[0].x_studio_field_DGArF
+                // // inParams.push(['id', 'name','display_name','res_name']);
+                // params.push(inParams);
+                
+                // odoo.execute_kw('sale.subscription', 'search_read', params, function(err,value){
+                //     if (err) { return console.log(err); }
+                //     let yourDate = new Date()
+                //     let formatedDate = yourDate.toISOString().split('T')[0]
+                //     if(new Date(value[0].recurring_next_date) >= new Date(formatedDate)){
+                //         res.status(200).json({
+                //             status: "success",
+                //             length: value?.length,
+                //             data: value,
+                //             subscription: true,
+                //             NIP: NIP
+                //         });
+                //     }else{
+                //         res.status(200).json({
+                //             status: "success",
+                //             length: value?.length,
+                //             // data: value,
+                //             message: "La suscripcion ha vencido!",
+                //             subscription: false
+                //         });
+                //     }
+                    
+                //     console.log(value)
+                       
+                // })
                 
             });
     
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 //test
 const http = require('http'); // or 'https' for https:// URLs
 const fs = require('fs');
+const { resolve } = require('path');
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2)
@@ -99,89 +100,48 @@ const odoo = new Odoo({
 
 //           });
 //     });
-    odoo.connect(function (err,value){
-    var finalArray = []
-    if(err) {return console.log(err)}
-    console.log('Connected to Odoo server')
-        var inParams = [];
-        inParams.push([['product_id.id', '=', 4],['partner_id.id', '=', '186804']]);
-        inParams.push(['name','confirmation_date','partner_id','user_id','amount_total',
-            'invoice_status', 'subscription_management', 'partner_invoice_id', 'orderhdr_id', 'order_line', 
-            'x_studio_field_DGArF', 'delivery_method_id', 'partner_shipping_id', 'cfdi_usage_id', 'origin','product_id']);
-        var params = [];
-        params.push(inParams);
-        odoo.execute_kw('sale.order', 'search_read', params, function (err, value) {
-          if (err) { return console.log(err); }
-            let test = value.filter(val => val.product_id[0] == 4 && val.x_studio_field_DGArF && val.origin)
-            // console.log("Numero de : ",test.length)
-            // console.log(test)
-            var filteredArray = test
-            
-            // inParams = []
-            // params = []
-            // // console.log(test.origin)
-            // inParams.push([['name', '=', test[0].origin]]);
-            // inParams.push(['date_start', 'recurring_next_date']);
-            // params.push(inParams);
-            test.map(x => {
-              // console.log(x)
-              inParams = []
-              params = []
-              // console.log(test.origin)
-              inParams.push([['name', '=', x.origin]]);
-              inParams.push(['date_start', 'recurring_next_date']);
-              params.push(inParams);
-              odoo.execute_kw('sale.subscription', 'search_read', params, function(err,value){
-                if (err) { return console.log(err); }
-                // console.log(value)
-                let yourDate = new Date()
-                let formatedDate = yourDate.toISOString().split('T')[0]
-                if(value >= formatedDate){
-                  // console.log(value[0])
-                  finalArray.push(value[0])
-                  // console.log(value[0])
-                  console.log('regreso suscripcion valida')
-                    // res.status(200).json({
-                    //     status: "success",
-                    //     length: value?.length,
-                    //     data: value,
-                    // });
-                }else{
-                  // console.log('regreso suscripcion vencida')
-                  filteredArray = filteredArray.filter(function(e) {return e.origin !== x.origin})
-                    // res.status(299).json({
-                    //     status: "success",
-                    //     length: value?.length,
-                    //     data: value,
-                    //     message: "La suscripcion ha vencido!"
-                    // });
-                }
-              })
-            })
-            
-            // odoo.execute_kw('sale.subscription', 'search_read', params, function(err,value){
-            //     if (err) { return console.log(err); }
-            //     console.log(value)
-            //     let yourDate = new Date()
-            //     let formatedDate = yourDate.toISOString().split('T')[0]
-            //     if(value[0].recurring_next_date >= formatedDate){
-            //       console.log('regreso suscripcion valida')
-            //         // res.status(200).json({
-            //         //     status: "success",
-            //         //     length: value?.length,
-            //         //     data: value,
-            //         // });
-            //     }else{
-            //       console.log('regreso suscripcion vencida')
-            //         // res.status(299).json({
-            //         //     status: "success",
-            //         //     length: value?.length,
-            //         //     data: value,
-            //         //     message: "La suscripcion ha vencido!"
-            //         // });
-            //     }
-            // })
-
-          });
-          console.log('resultado final: ', finalArray)
-    });
+// odoo.connect(function (err,value){
+//   var finalArray = []
+//   // var test = []
+//   if(err) {return console.log(err)}
+//   console.log('Connected to Odoo server')
+//     var inParams = [];
+//     inParams.push([['product_id.id', '=', 4],['partner_id.id', '=', '186804']]);
+//     inParams.push(['name','confirmation_date','partner_id','user_id','amount_total',
+//         'invoice_status', 'subscription_management', 'partner_invoice_id', 'orderhdr_id', 'order_line', 
+//         'x_studio_field_DGArF', 'delivery_method_id', 'partner_shipping_id', 'cfdi_usage_id', 'origin','product_id']);
+//     var params = [];
+//     params.push(inParams);
+//     odoo.execute_kw('sale.order', 'search_read', params, function (err, value) {
+    
+//       if (err) { return console.log(err); }
+//         var test = value.filter(val => val.product_id[0] == 4 && val.x_studio_field_DGArF && val.origin)
+//         var filteredArray = test
+//         function callBack () {
+//           console.log('Final: ', finalArray)
+//         }
+//         var itemsProcessed = 0;
+//         test.forEach(item => {
+//           inParams = []
+//             params = []
+//             // console.log(test.origin)
+//             inParams.push([['name', '=', item.origin]]);
+//             inParams.push(['date_start', 'recurring_next_date']);
+//             params.push(inParams);
+//             odoo.execute_kw('sale.subscription', 'search_read', params, function(err,value){
+//               if (err) { return console.log(err); }
+//               let yourDate = new Date()
+//               let formatedDate = yourDate.toISOString().split('T')[0]
+//               if(new Date(value[0].recurring_next_date) >= new Date(formatedDate)){
+//                 finalArray.push(value[0])
+//               }else{
+//                 filteredArray = filteredArray.filter(function(e) {return e.origin !== x.origin})
+//               }
+//               itemsProcessed++;
+//               if(itemsProcessed === test.length) {
+//                 callBack();
+//               }
+//             })
+//         });
+//       });
+//     });
